0b4cbf06ca727799e0ea5b06b4ca17c31bea34bc
Emily <emily@emilyde-macbook-pro.local>
2018-07-13 13:50:03 +0800

    Initial Commit

---
+++ Emily' Music list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emily' Music list
+//
+//  Created by Emily on 2018/7/13.
+//  Copyright © 2018年 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
